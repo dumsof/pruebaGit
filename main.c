6ff20a3d1a9491057c5a5c8bc8470a0bda2e2c99
darwinum <dun34@hotmail.com>
2019-03-12 13:02:53 -0500

    se realiza una modificacion en la linea 4 y 5

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #estoy agregando una nueva linea master
 int main(int argc, char** argv){
-		printf("hola, mundo. \n");
+		printf("hola, mundo se cambia para marge. \n");
+		printf("esta es una nueva linea. \n");
 		return 0;
 
 }
